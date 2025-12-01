91149939906bcbbafb1c8fcff8704fe9053b4cf3
F9NNEC <adityatriandriyansyah9@gmail.com>
2025-12-02 00:17:18 +0700

    multi-auth (admin, user)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,7 +14,11 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        if (Auth::user()->role === 'admin') {
+            return view('admin.dashboard');
+        } else {
+            return view('user.dashboard');
+        }
     })->name('dashboard');
 
     Route::get('/books', [BookController::class, 'index'])->name('books.collections');
