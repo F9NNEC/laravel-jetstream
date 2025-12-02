93de5b5e7779da7162a6b2aceca26048e78d0888
F9NNEC <adityatriandriyansyah9@gmail.com>
2025-12-02 12:38:26 +0700

    artikel card, comment

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -4,7 +4,7 @@
             {{ __('User Dashboard') }}
         </h2>
     </x-slot>
-
+    
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
@@ -12,6 +12,78 @@
                     <h3 class="text-lg font-medium text-gray-900">Welcome, User!</h3>
                     <p class="mt-2 text-sm text-gray-600">Halooooooooo.</p>
                 </div>
+                
+                <x-welcome />
+                
+                <div class="py-12 sm:px24 lg:px-32">
+                    <article class="relative overflow-hidden rounded-lg shadow-sm transition hover:shadow-lg">
+                        <img alt="" src="https://images.unsplash.com/photo-1661956602116-aa6865609028?auto=format&amp;fit=crop&amp;q=80&amp;w=1160" class="absolute inset-0 h-full w-full object-cover">
+    
+                        <div class="relative bg-linear-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
+                            <div class="p-4 sm:p-6">
+                            <time datetime="2022-10-10" class="block text-xs text-white/90"> 10th Oct 2022 </time>
+    
+                            <a href="#">
+                                <h3 class="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
+                            </a>
+    
+                            <p class="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
+                                pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
+                                quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
+                                atque dignissimos. Molestias explicabo corporis voluptatem?
+                            </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>                
+
+                <div class="py-12 sm:px24 lg:px-32">
+                    <article class="relative overflow-hidden rounded-lg shadow-sm transition hover:shadow-lg">
+                        <img alt="" src="https://images.unsplash.com/photo-1661956602116-aa6865609028?auto=format&amp;fit=crop&amp;q=80&amp;w=1160" class="absolute inset-0 h-full w-full object-cover">
+    
+                        <div class="relative bg-linear-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
+                            <div class="p-4 sm:p-6">
+                            <time datetime="2022-10-10" class="block text-xs text-white/90"> 10th Oct 2022 </time>
+    
+                            <a href="#">
+                                <h3 class="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
+                            </a>
+    
+                            <p class="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
+                                pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
+                                quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
+                                atque dignissimos. Molestias explicabo corporis voluptatem?
+                            </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+
+                <div class="py-12 sm:px24 lg:px-32">
+                    <article class="relative overflow-hidden rounded-lg shadow-sm transition hover:shadow-lg">
+                        <img alt="" src="https://images.unsplash.com/photo-1661956602116-aa6865609028?auto=format&amp;fit=crop&amp;q=80&amp;w=1160" class="absolute inset-0 h-full w-full object-cover">
+    
+                        <div class="relative bg-linear-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
+                            <div class="p-4 sm:p-6">
+                            <time datetime="2022-10-10" class="block text-xs text-white/90"> 10th Oct 2022 </time>
+    
+                            <a href="#">
+                                <h3 class="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
+                            </a>
+    
+                            <p class="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
+                                pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
+                                quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
+                                atque dignissimos. Molestias explicabo corporis voluptatem?
+                            </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+
             </div>
         </div>
     </div>
