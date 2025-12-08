392ff2a8b5aa72201f8fcda33db5dee5c30d8557
F9NNEC <adityatriandriyansyah9@gmail.com>
2025-12-08 22:33:31 +0700

    update ui

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
+                        <x-application-mark class="block h-8 w-auto" />
                     </a>
                 </div>
 
@@ -20,6 +20,12 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('articles.index') }}" :active="request()->routeIs('Articles')">
+                        {{ __('Articles') }}
                     </x-nav-link>
                 </div>
 
